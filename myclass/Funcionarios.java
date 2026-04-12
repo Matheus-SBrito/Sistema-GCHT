98e5b2ca0ab49a57e7e3733877d949d8219be77e
Matheus-SBrito <matheuscgpe@gmail.com>
2026-04-12 14:39:31 -0300

    Criação da Base de Dados dos Usuários Classe Funcionarios
    O commit da vez traz as seguintes alterações:
    
    	- Criação da pasta 'myclass'.
    	Tem a função de englobar todas as classes
    	bases para o registro de todas as 'entidades'
    	presentes no sistema.
    
    	- Criação da classe 'Funcionarios'.
    	A classe funcionarios é será a base, a
    	herança de outras duas classes que
    	definirão os diferentes tipos de usuários
    	que utilizar]ao o sistema.

---
+++ myclass/Funcionarios.java
@@ -0,0 +1,55 @@
+public class Funcionarios{
+
+    private String NDF;
+    private String cpf;
+    private String nome;
+    private String sobrenome;
+    private int idade;
+
+    private String setor;
+    private boolean emAtividade;
+    
+
+    public Funcionarios(
+        String cpf,
+        String nome,
+        String sobrenome,
+        int idade,
+
+        String setor,
+        boolean emAtividade
+    ){
+
+        this.cpf = cpf;
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.idade = idade;
+
+        this.setor = setor;
+        this.emAtividade = emAtividade;
+
+        this.NDF = montadorNDF();
+        
+    }
+
+    public String getNome (){return this.nome;}
+    public void setterNome (String nome){this.nome = nome;}
+
+    public String getSobrenome (){return this.sobrenome;}
+    public void setterSobrenome (String sobrenome){this.sobrenome = sobrenome;}
+    
+    public int getIdade (){return idade;}
+    public void setterIdade (int idade){this.idade = idade;}
+
+    public String getSetor (){return this.setor;}
+    public void setter (String setor){this.setor = setor;}
+
+    public String getNDF (){return this.NDF;}
+    private static String montadorNDF(){return "1";}
+
+    public String getCPF(){return this.cpf;}
+
+    public boolean getEmAtividade(){return this.emAtividade;}
+    public void setterEmAtividade(boolean emAtividade){this.emAtividade = emAtividade;}
+    
+}
